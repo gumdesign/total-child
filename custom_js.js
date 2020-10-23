20b91991177fd3fdeb51c0c07b23073dc074f658
gumdesign <christian@gumdesign.com>
2020-10-23 09:53:20 -1000

    add vc_row toggle

--- custom_js.js
+++ custom_js.js
@@ -1,5 +1,37 @@
-
 jQuery(document).ready(function($) {
     // $() will work as an alias for jQuery() inside of this function
     //  $('.slides').append('<div class="banner"></div>');
+
+    // add external icon to all outgoing links
+    $("#primary a").filter(function() {
+    return this.hostname && this.hostname !== location.hostname;
+    }).attr('target', '_blank').append(' <span class="ticon ticon-external-link"></span>');
 });
+
+
+/**
+ * show/hide VC row
+ */
+( function( $ ) {
+
+    'use strict';
+
+    $( document ).ready( function() {
+
+        // 
+        var $trigger = $( '.vc-row-toggle-button' );
+
+        // 
+        var $hiddenRow = $( '.vc-row-toggle' );
+
+        if ( $hiddenRow.length ) {
+            $trigger.click( function() {
+                console.log('click vc-row-toggle-button');
+                $hiddenRow.slideToggle();
+                return false;
+            } );
+        }
+
+    } );
+
+} ( jQuery ) );
